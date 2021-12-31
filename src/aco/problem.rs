d4be906576ca310d184e54856a123dafd476c519
ShaneEverittM <shaneeverittmurphy@gmail.com>
2021-12-31 01:34:01 -0800

    Improve parsing readability

--- src/aco/problem.rs
+++ src/aco/problem.rs
@@ -2,15 +2,12 @@ use std::{
     fs::File,
     io::Read,
     str::FromStr,
-    num::ParseIntError,
+    num::ParseFloatError,
 };
 
 use anyhow::{anyhow, Result};
 use strum::EnumString;
-
-pub use matrix::Matrix;
-
-mod matrix;
+use yoos::collections::Matrix;
 
 
 #[allow(dead_code)]
@@ -33,54 +30,53 @@ impl Problem {
     fn parse(i: &str) -> NomResult<&str, Self> {
         // Local use statement so as not to clutter top of file, we need many
         use nom::{
-            combinator::{opt, map, map_res, map_parser},
+            error::ParseError,
+            IResult,
+            combinator::{map_res, map_parser},
             bytes::complete::{tag, take_until1, take_while1},
-            sequence::{delimited, preceded, tuple, pair},
-            character::complete::{digit1, space1, line_ending},
-            multi::{many0, separated_list1, count},
+            sequence::{terminated, delimited, preceded, tuple, separated_pair},
+            character::complete::{digit1, space0, space1, line_ending},
+            multi::count,
         };
 
         /******************************/
         /*        Helper parsers      */
         /******************************/
 
-        // End of lines sometimes have trailing spaces. 
-        // This is a macro since taking function as argument
-        // is overly complicated when we can just copy paste.
-        macro_rules! trailing_ws {
-            ($inner:expr) => {
-                nom::sequence::terminated(
-                    $inner, 
-                    |input| preceded(opt(many0(tag(" "))), line_ending)(input)
-                )
-            } 
-        }
-
-        // Key followed by colon, macro for same reason as above.
-        macro_rules! key_then {
-            ($key:expr, $inner:expr) => {
-                nom::sequence::preceded(
-                    pair(tag($key), tag(" : ")),
-                    $inner, 
-                )
-            } 
+        // Applies the inner parser, then consumes any number of spaces then a line ending
+        fn trailing_ws<'a, F, O, E>(inner: F) -> impl FnMut(&'a str) -> IResult<&'a str, O, E>
+            where
+                F: FnMut(&'a str) -> IResult<&'a str, O, E> + 'a,
+                E: ParseError<&'a str> + 'a
+        {
+            terminated(inner, preceded(space0, line_ending))
         }
 
         // Single word value after "<key>:"
-        let word_after = |key| {
-            let word = take_while1(
-                |c: char| {
-                    c.is_ascii_alphanumeric() || c.is_ascii_punctuation()
-                }
-            );
-            key_then!(key, trailing_ws!(word))
-        };
+        fn word_after<'a, E>(key: &'a str) -> impl FnMut(&'a str) -> IResult<&'a str, &'a str, E>
+            where
+                E: ParseError<&'a str> + 'a
+        {
+            preceded(
+                terminated(tag(key), tag(" : ")),
+                trailing_ws(take_while1(
+                    |c: char| {
+                        c.is_ascii_alphanumeric() || c.is_ascii_punctuation()
+                    }
+                )),
+            )
+        }
 
         // Paren-delimited sentence after "<key>:"
-        let comment_after = |key| {
-            let between_parens = delimited(tag("("), take_until1(")"), tag(")"));
-            key_then!(key, trailing_ws!(between_parens))
-        };
+        fn comment_after<'a, E>(key: &'a str) -> impl FnMut(&'a str) -> IResult<&'a str, &'a str, E>
+            where
+                E: ParseError<&'a str> + 'a
+        {
+            preceded(
+                terminated(tag(key), tag(" : ")),
+                trailing_ws(delimited(tag("("), take_until1(")"), tag(")"))),
+            )
+        }
 
         /******************************/
         /*       Actual Parsing       */
@@ -116,44 +112,41 @@ impl Problem {
             usize::from_str,
         )(i)?;
 
+        // One coordinate triplet
+        let coordinate = map_res(
+            trailing_ws(
+                preceded(
+                    tuple((space1, digit1, space1)),
+                    separated_pair(digit1, space1, digit1),
+                )
+            ),
+            |(x, y): (&str, &str)| -> Result<_, ParseFloatError> {
+                Ok((x.parse::<f64>()?, y.parse::<f64>()?)) // Discard unneeded values
+            },
+        );
+
         // After the header, get exactly <dimension> tuples of 3 digits separated by spaces,
         // map them to NodeCoordinates, then Matrix
-        let (i, adjacency_matrix) = map(
-            preceded(
-                trailing_ws!(tag("NODE_COORD_SECTION")),
-                count(
-                    map_res(
-                        map(
-                            trailing_ws!(
-                            preceded(space1, tuple((digit1, space1, digit1, space1, digit1)))
-                        ),
-                            |f| (f.2, f.4), // Discard unneeded values
-                        ),
-                        NodeCoordinate::from_tuple,
-                    ),
-                    dimension,
-                ),
-            ),
-            Matrix::from,
+        let (i, coordinates) = preceded(
+            trailing_ws(tag("NODE_COORD_SECTION")),
+            count(coordinate, dimension),
         )(i)?;
+
+        // One demand
+        let demand = map_res(
+            trailing_ws(preceded(tuple((digit1, space1)), digit1)),
+            usize::from_str,
+        );
 
         // After the header, get exactly <dimension> pairs of values, 
         // mapping the second of which to a demand value
         let (i, demands) = preceded(
-            trailing_ws!(tag("DEMAND_SECTION")),
-            count(
-                map_res(
-                    trailing_ws!(separated_list1(space1, digit1)),
-                    |v: Vec<&str>| -> Result<_, ParseIntError> {
-                        usize::from_str(v[1])
-                    },
-                ),
-                dimension,
-            ),
+            trailing_ws(tag("DEMAND_SECTION")),
+            count(demand, dimension),
         )(i)?;
 
         Ok((i, Self {
-            adjacency_matrix,
+            adjacency_matrix: Matrix::adjacency(coordinates),
             demands,
             capacity,
             dimension,
@@ -229,33 +222,6 @@ impl EdgeWeightType {
     }
 }
 
-#[derive(Default)]
-struct NodeCoordinate {
-    x: f64,
-    y: f64,
-}
-
-impl NodeCoordinate {
-    pub fn from_tuple(v: (&str, &str)) -> Result<Self> {
-        Ok(NodeCoordinate { x: v.0.parse()?, y: v.1.parse()? })
-    }
-
-    pub fn distance_from(&self, other: &NodeCoordinate) -> f64 {
-        ((other.y - self.y).powi(2) + (other.x - self.x).powi(2)).sqrt()
-    }
-}
-
-impl From<Vec<NodeCoordinate>> for Matrix {
-    fn from(coordinates: Vec<NodeCoordinate>) -> Self {
-        let mut matrix = Matrix::new(coordinates.len());
-        for (i, a) in coordinates.iter().enumerate() {
-            for (j, b) in coordinates.iter().enumerate() {
-                matrix[i][j] = a.distance_from(b);
-            }
-        }
-        matrix
-    }
-}
 
 #[cfg(test)]
 mod tests {
